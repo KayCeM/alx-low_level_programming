dd2d066e3e37c4aaf52dab2616aba0533fb7b024
KayCeM <kgomotsocomfortmaduna@outlook.com>
2023-09-25 23:09:48 -0700

    fixed

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,17 +1,18 @@
 #include "main.h"
 /**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
+ *_memcpy - a function that copies memory area
+ *@dest: memory where is stored
+ *@src: memory where is copied
+ *@n: number of bytes
  *
- * Return: Nothing.
+ *Return: copied memory with n bytes changed
  */
- char *_memcpy(char *dest, char *src, unsigned int n)
+char *_memcpy(char *dest, char *src, unsigned int n)
 {
 	int r = 0;
 	int i = n;
 
-	for (;r < i; r++)
+	for (; r < i; r++)
 	{
 		dest[r] = src[r];
 		n--;
