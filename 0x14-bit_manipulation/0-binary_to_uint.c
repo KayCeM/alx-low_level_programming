9fab33d5215cb5bf2a80abdfb75af02d34a8d1dd
KayCeM <Kgomotsocomfortmaduna@outlook.com>
2024-02-23 05:43:34 -0800

    updates

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,37 +1,29 @@
-#include <stdio.h>
-
 #include "main.h"
+
 /**
- * binary_to_unit - program that converts a binary to intagers/decimal numbers
+ * binary_to_uint - function converts a binary number to an unsigned int.
+ * @b: pointer to a string containing a binary number
  *
- * @b: a character pointer to a string containing the binary numbers
- *
- * Return: the converted decimal (output)
+ * Return: unsigned int with decimal value of binsry number, or 0 if error
  */
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int len, i, j, sum, pow;
-	int base;
+	int a;
+	unsigned int num;
 
-	base = 2;
-	sum = 0;
-	pow = 1;
-	if (b == NULL)
+	num = 0;
+	if (!b)
 		return (0);
-	for (len = 0; b[len] != '\0'; len++
-
-	if (len == 1 && (b[0] == '0' || b[0] == '1'))
-		return (b[0] = 48);
-	for (i = 0; b[i] != '\0'; i++)
+	for (a = 0; b[a] != '\0'; a++)
 	{
-		if (b[i] != '0' && b[i] != 'i')
+		if (b[a] != '0' && b[a] != '1')
 			return (0);
-		for (j = len = 1; j > 0; j--)
-			pow = pow * base;
-		sum = sum + (pow * (b[i] - 48));
-		len--;
-		pow = 1;
-
 	}
-return (sum);
+	for (a = 0; b[a] != '\0'; a++)
+	{
+		num <<= 1;
+		if (b[a] == '1')
+			num += 1;
+	}
+	return (num);
 }
